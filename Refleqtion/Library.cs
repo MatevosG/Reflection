dc2fc328af0a293765738966f2565cc65fe086a3
LENOVO <LENOVO@DESKTOP-V6FI0B2>
2021-10-18 21:28:10 +0400

    Add project files.

---
+++ Refleqtion/Library.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    public class Library
+    {
+        public Library()
+        {
+
+        }
+        public Library(string bookname, string author, int pagecount)
+        {
+            _bookname = bookname;
+            _author = author;
+            _pagecount = pagecount;
+        }
+
+        public string _bookname { get; }
+        public string _author { get; }
+        public int _pagecount { get; }
+        private void Print(List<Library> library)
+        {
+            for (int i = 0; i < library.Count; i++)
+            {
+                Console.WriteLine($"bookname - {library[i]._bookname}\n author - {library[i]._author}\n pagecount - {library[i]._pagecount} ");
+            }
+        }
+        public static bool TryGetAuthor(List<Library> library, string name)
+        {
+
+            bool hasauthor = false;
+
+            for (int i = 0; i < library.Count; i++)
+            {
+                if (library[i]._author == name)
+                {
+                    name = library[i]._author;
+                    hasauthor = true;
+                    break;
+                }
+            }
+            return hasauthor;
+        }
+        public static bool TryGetBookName(List<Library> library, string name)
+        {
+
+            bool hasbook = false;
+
+            for (int i = 0; i < library.Count; i++)
+            {
+                if (library[i]._bookname == name)
+                {
+                    name = library[i]._bookname;
+                    hasbook = true;
+                    break;
+                }
+            }
+            return hasbook;
+        }
+    }
+}
---
+++ Refleqtion/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Library library = new Library("ggg", "ggggg gggggggg", 550);
+
+            Type mytype = typeof(Library);
+
+            PropertyInfo[] properties = mytype.GetProperties();
+
+            for (int i = 0; i < properties.Length; i++)
+            {
+                Console.WriteLine(properties[i].GetValue(library));
+                Console.WriteLine(properties[i].Name);
+            }
+
+            MethodInfo[] methods = mytype.GetMethods();
+
+            for (int i = 0; i < methods.Length; i++)
+            {
+                Console.WriteLine(methods[i].Name);
+            }
+            var res9 = methods[4].GetParameters();
+            bool res7 = methods[5].IsAbstract;
+            bool res8 = methods[4].IsStatic;
+
+            MemberInfo[] members = mytype.GetMembers();
+
+            for (int i = 0; i < members.Length; i++)
+            {
+                Console.WriteLine(members[i].Name);
+            }
+
+            ConstructorInfo[] constructors = mytype.GetConstructors();
+
+            for (int i = 0; i < constructors.Length; i++)
+            {
+                Console.WriteLine(constructors[i].Name);
+            }
+
+            var res = mytype.GetConstructors()[1].GetParameters()[1].Name;
+            Console.WriteLine(res);
+            object res1 = mytype.GetConstructors()[1].Invoke(new object[] { "lllll", "lll llllll", 87 });
+            mytype.GetConstructors()[1].Invoke(library, new object[] { "lllll", "lll llllll", 87 });
+            var res4 = mytype.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+            var res5 = mytype.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+            var res6 = mytype.GetMethods()[0].Invoke(library, null);
+
+
+        }
+    }
+   
+
+}
